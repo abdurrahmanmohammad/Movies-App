994661b28a5415dd6f739c01e47e918eaaff2d34
abdurrahmanmohammad <abdurrahmanmohammad02@gmail.com>
2021-09-02 16:16:44 -0700

    Implemented API movie retrieval & created Movie model to store API data

--- app/src/main/java/com/example/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/moviesapp/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.moviesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.moviesapp.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=04387a9831174fd21d2ca3db9bdd8ca6";
+    public static final String TAG = "MainActivity"; // For debugging
+    List<Movie> movies;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject; // Get the actual JSON object
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results"); // Get results array from JSON object
+                    Log.i(TAG, "Results: " + results.toString());
+                    movies = Movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit JSON exception", e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/moviesapp/models/Movie.java
@@ -0,0 +1,46 @@
+package com.example.moviesapp.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    private String base_url = "https://image.tmdb.org/t/p";
+    private String size = "w342";
+    private String poster_path;
+    private String title;
+    private String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        // Set variables from passed in objects
+        // If fields do not exist in JSON object, throw an exception - caller should deal with the thrown exception
+        poster_path = String.format("%s/%s/%s", base_url, size, jsonObject.getString("poster_path"));
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    // Create a list of Movie objects from a JSON array
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>(); // Initialize the return object
+        // Iterate throught the JSON array and append a new Movie object for each entry
+        for (int i = 0; i < movieJsonArray.length(); i++)
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        return movies; // Return a list of Movie objects containing API JSON data
+    }
+
+    // ******************** Getters ********************
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/%s/%s", size, poster_path);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
