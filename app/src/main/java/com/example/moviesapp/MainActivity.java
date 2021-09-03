31b699451d7ecf9daaf86d5b08264a7fb7a7deac
abdurrahmanmohammad <abdurrahmanmohammad02@gmail.com>
2021-09-03 07:18:38 -0700

    Improved API call: refractored into a function with Volley

--- app/src/main/java/com/example/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/moviesapp/MainActivity.java
@@ -5,48 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import com.codepath.asynchttpclient.AsyncHttpClient;
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.moviesapp.adapters.MovieAdapter;
-import com.example.moviesapp.models.Movie;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
-
     public static final String TAG = "MainActivity"; // For debugging
-    List<Movie> movies;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        movies = new ArrayList<>(); // Initialize movies list
-
         RecyclerView moviesRecyclerView = findViewById(R.id.movies_recycler_view);
-
         // Bind adapter to data source to populate recycler view
-        // Step 1: Create an adapter
-        MovieAdapter movieAdapter = new MovieAdapter(this);
-        // Step 2: Set the adapter on the recycler view
-        moviesRecyclerView.setAdapter(movieAdapter);
-        // Step 3: Set a layout manager on the recycler view
-        moviesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
-
-
-
+        MovieAdapter movieAdapter = new MovieAdapter(this); // Step 1: Create an adapter
+        moviesRecyclerView.setAdapter(movieAdapter); // Step 2: Set the adapter on the recycler view
+        moviesRecyclerView.setLayoutManager(new LinearLayoutManager(this)); // Step 3: Set a layout manager on the recycler view
     }
 }
--- app/src/main/java/com/example/moviesapp/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/moviesapp/adapters/MovieAdapter.java
@@ -11,9 +11,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
-import com.codepath.asynchttpclient.AsyncHttpClient;
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.moviesapp.R;
 import com.example.moviesapp.models.Movie;
 
@@ -24,19 +28,21 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-import okhttp3.Headers;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
     public static final String TAG = "MovieAdapter"; // For debugging
-    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=04387a9831174fd21d2ca3db9bdd8ca6";
-
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/popular";
+    public static final String API_KEY = "04387a9831174fd21d2ca3db9bdd8ca6";
+    private RequestQueue mQueue; // Request queue for API requests
+    private int page = 1; // Initially load page 1 of API response
 
     Context context; // Context to inflate the views - where the adapter is being constructed from
     List<Movie> movies = new ArrayList<>(); // List of movies the adapter needs to hold on to
 
     public MovieAdapter(Context context) {
         this.context = context;
-        getMovies(); // Initialize movies
+        mQueue = Volley.newRequestQueue(context); // Instantiate the RequestQueue
+        getMovies(page); // Initialize movies
     }
 
     @NonNull
@@ -62,30 +68,36 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         return movies.size(); // The number of items is the number of movies in the list
     }
 
-    private void getMovies() {
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Headers headers, JSON json) {
-                Log.d(TAG, "onSuccess");
-                JSONObject jsonObject = json.jsonObject; // Get the actual JSON object
-                try {
-                    JSONArray results = jsonObject.getJSONArray("results"); // Get results array from JSON object
-                    Log.i(TAG, "Results: " + results.toString());
-                    movies.addAll(Movie.fromJsonArray(results));
-                    notifyDataSetChanged(); // Whenever data changes, re-render view
-                    Log.i(TAG, "Movies: " + movies.size());
-                } catch (JSONException e) {
-                    Log.e(TAG, "Hit JSON exception", e);
-                    e.printStackTrace();
-                }
-            }
+    private void getMovies(int page) {
+        String url = String.format("%s?api_key=%s&page=%d", NOW_PLAYING_URL, API_KEY, page);
+        // Request a JSON object response from the API URL
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, "onSuccess");
+                        try {
+                            JSONArray results = response.getJSONArray("results"); // Get results array from JSON object
+                            Log.i(TAG, "Results: " + results.toString());
+                            movies.addAll(Movie.fromJsonArray(results));
+                            notifyDataSetChanged(); // Whenever data changes, re-render view
+                            Log.i(TAG, "Movies: " + movies.size());
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Hit JSON exception", e);
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "onFailure");
+                        Log.e(TAG, "Hit VolleyError exception", error);
+                        error.printStackTrace();
+                    }
+                });
 
-            @Override
-            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.d(TAG, "onFailure");
-            }
-        });
+
+        mQueue.add(jsonObjectRequest); // Add the request to the RequestQueue
     }
 
 
